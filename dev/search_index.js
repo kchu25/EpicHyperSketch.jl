668a09c618456ff7dac5a2cfa982755568d0048e
Documenter.jl <documenter@juliadocs.github.io>
2025-09-27 20:44:27 +0000

    build based on 8d64f63

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#EpicHyperSketch","page":"Home","title":"EpicHyperSketch","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for EpicHyperSketch.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
